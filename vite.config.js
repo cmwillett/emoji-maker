2b6622c6171368f35469eb9b350e4cf3a2528195
Craig Willett <craig.willett@gmail.com>
2025-05-27 14:27:21 -0400

    adding background color options

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
     UnoCSS(),
