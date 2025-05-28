b513dec99606487eeeedeb982ba018c7ee0eea84
Craig Willett <craig.willett@gmail.com>
2025-05-27 22:45:39 -0400

    working on counter

--- src/App.jsx
+++ src/App.jsx
@@ -184,7 +184,7 @@ export default function App() {
       <h1 className ="text-3xl font-bold text-emerald-400 drop-shadow-lg">The Craig's</h1> 
       <h1 className="text-3xl font-bold text-emerald-400 drop-shadow-lg">Emoji Maker</h1>
       {emojiCount !== null && (
-        <p className="text-sm text-emerald-400 mt-2">
+        <p className="text-sm text-emerald-400 mt-2 font-bold underline">
           {emojiCount.toLocaleString()} emojis created so far!
         </p>
       )}
