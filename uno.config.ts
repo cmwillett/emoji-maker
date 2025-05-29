943a71cf418672cb121eb7fc1c0b155d92a6709d
Craig Willett <craig.willett@gmail.com>
2025-05-29 10:12:23 -0400

    added share button

--- uno.config.ts
+++ uno.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, presetUno } from 'unocss'
 export default defineConfig({
   presets: [presetUno()],
   shortcuts: [
-    ['btn-primary', 'px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 shadow cursor-pointer transition-colors'],
+    ['btn-primary', 'px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 shadow cursor']
   ]
 })
