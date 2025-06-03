fe1303d5a746d196c5f02d383e0ef8a013bf86ba
Craig Willett <craig.willett@gmail.com>
2025-06-03 15:09:32 -0400

    added a logo to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header({ emojiCount }) {
     <div className="bg-black/40 border border-emerald-400 rounded-lg p-2 mb-4">
       <div className="flex flex-col items-center space-y-1">
         <h1 className="text-3xl font-bold text-emerald-400 drop-shadow-lg underline mt-0 mb-0">The Craig's</h1>
+        <img src="/logo.png" width="150" height="150"/>
         <h1 className="text-3xl font-bold text-emerald-400 drop-shadow-lg underline mt-0">Emoji/Meme Maker</h1>
         {emojiCount !== null && (
           <p className="text-sm text-emerald-400 mt-2 font-bold underline">
